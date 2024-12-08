3d82fcf046db93e5b88fd1d79a42ca153f2f28ce
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2024-12-08 12:14:52 -0500

    Email Trigger using FluentEmail and Papercut

---
+++ FluentEmail_PaperCutApp/EmailSenderApi/Controllers/EmailController.cs
@@ -0,0 +1,36 @@
+﻿using FluentEmail.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailSenderApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmailController : ControllerBase
+{
+    private readonly IFluentEmail _fluentEmail;
+    private readonly ILogger<EmailController> _logger;
+
+    public EmailController(IFluentEmail fluentEmail, ILogger<EmailController> logger)
+    {
+        _fluentEmail = fluentEmail;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Send(EmailSendRequest request)
+    {
+        _logger.LogInformation("Sending Email using FluentEmail");
+        var response = await _fluentEmail
+                                .To(request.ToAddress)
+                                .Subject(request.Subject)
+                                .Body(request.Message, isHtml: true)
+                                .SendAsync();
+
+        return (response.Successful) ? Ok(response) : BadRequest();
+    }
+}
+
+
+public record EmailSendRequest(string ToAddress, string Subject, string Message);
---
+++ FluentEmail_PaperCutApp/EmailSenderApi/READMe.md
@@ -0,0 +1,15 @@
+## Send Email using FluentEmail and PaperCut in .Net App
+
+- Using docker-compose setup a service for Papercut.
+- Include the Nuget package for FluentEmail.Smtp
+- Setup the configuration in the appSettings and configure it in Program.cs
+- Define a record for email send request
+- Using FluentEmail interface send the email.
+- Verify the email in the papercut.
+
+![alt text](image-2.png)
+
+![alt text](image-1.png)
+
+![alt text](image.png)
+
