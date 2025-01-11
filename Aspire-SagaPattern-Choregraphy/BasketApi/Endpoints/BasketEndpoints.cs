f98397ea42373cba5477c47d82ce19f00d6ff4d8
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-01-11 18:59:36 -0500

    Added RabbitMq

--- Aspire-SagaPattern-Choregraphy/BasketApi/Endpoints/BasketEndpoints.cs
+++ Aspire-SagaPattern-Choregraphy/BasketApi/Endpoints/BasketEndpoints.cs
@@ -2,6 +2,7 @@
 using BasketApi.Models;
 using Carter;
 using Microsoft.AspNetCore.Mvc;
+using SharedLib.Enums;
 using SharedLib.Repository;
 
 namespace BasketApi.Endpoints;
@@ -21,7 +22,11 @@ public class BasketEndpoints : CarterModule
     {
 
         logger.LogInformation("Received the new bucket item");
+        var basket = Basket.Create(request.CustomerId, SharedLib.Enums.BasketStatus.Uncertain);
 
-
+        request.BasketItems.ForEach(basketItem => basket.AddBasketItem(basketItem.ProductId, basketItem.ProductId, basketItem.Price, basketItem.Count, BasketStatus.Uncertain));
+        await repository.AddAsync(basket);
+        await repository.SaveChangesAsync();
+        logger.LogInformation("Created new bucket item");
     }
 }
