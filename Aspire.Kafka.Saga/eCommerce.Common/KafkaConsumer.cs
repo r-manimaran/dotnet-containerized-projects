c37036c89027dc44a861412e50e31e6ac24ac5e7
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-02-09 18:21:37 -0500

    Added KafkaProducer and KafkaConsumer Generic methods

---
+++ Aspire.Kafka.Saga/eCommerce.Common/KafkaConsumer.cs
@@ -0,0 +1,55 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Confluent.Kafka.ConfigPropertyNames;
+
+namespace eCommerce.Common;
+
+public class KafkaConsumer : BackgroundService
+{
+
+    private IConsumer<string,string> _consumer;
+    public KafkaConsumer(string[] topics, IConsumer<string,string> consumer)
+    {
+       _consumer = consumer;
+       _consumer.Subscribe(topics);
+    }
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.Run(() =>
+        {
+            _ = HandleConsume("order-topic", stoppingToken);
+        }, stoppingToken);
+    }
+
+    public async Task HandleConsume(string topic, CancellationToken stoppingToken)
+    {
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                var consumeResult = _consumer.Consume(stoppingToken);
+                await ConsumeAsync(consumeResult);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error processing message: {ex.Message}");
+
+            }
+        }
+        _consumer.Close();
+    }
+
+    protected virtual Task ConsumeAsync(ConsumeResult<string,string> consumeResult)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ Aspire.Kafka.Saga/eCommerce.Common/KafkaProducer.cs
@@ -0,0 +1,43 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Common;
+
+public class KafkaProducer : IKafkaProducer
+{
+    private readonly IProducer<string, string> _producer;
+    public KafkaProducer(IProducer<string, string> producer)
+    {
+        //var config = new ConsumerConfig
+        //{
+        //    GroupId = "order-group",
+        //    AutoOffsetReset = AutoOffsetReset.Earliest,
+        //    BootstrapServers = "localhost:29092"
+        //};
+        //_producer = new ProducerBuilder<string, string>(config).Build();
+        _producer = producer;
+    }
+    public async Task ProduceAsync(string topic, object message)
+    {
+        try
+        {
+            var kafkaMessage = new Message<string, string> { Value = JsonConvert.SerializeObject(message) };
+
+            await _producer.ProduceAsync(topic, kafkaMessage);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
+
+public interface IKafkaProducer
+{
+    Task ProduceAsync(string topic, object message);
+}
