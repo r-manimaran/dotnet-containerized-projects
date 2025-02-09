8c9d279a45d8042f1827203d5bcf2ebfe1a663b3
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-02-09 18:22:26 -0500

    Added Shared Lib project

---
+++ Aspire.Kafka.Saga/SharedLib/Constants/KafkaTopics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLib.Enums;
+
+public static class KafkaTopics 
+{
+    public const string ORDER_CREATED = "order-created";
+    public const string PRODUCTS_RESERVED = "products-reserved";
+    public const string PAYMENT_PROCESSED = "payment-processed";
+
+    public const string PRODUCTS_RESERVATION_FAILED = "products-reservation-failed";
+    public const string PRODUCTS_RESERVATION_CANCELED = "products-reservation-canceled";
+        
+    public const string PAYMENT_FAILED = "payment-failed";
+   
+}
---
+++ Aspire.Kafka.Saga/SharedLib/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLib.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string CustomerName { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string? Status { get; set; }
+
+}
