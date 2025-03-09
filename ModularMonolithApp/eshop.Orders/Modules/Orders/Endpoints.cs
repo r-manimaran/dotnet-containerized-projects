1c51478aa9895a09537fb827b6e250098e1bcffc
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-03-09 12:12:10 -0400

    Updated Endpoint

--- ModularMonolithApp/eshop.Orders/Modules/Orders/Endpoints.cs
+++ ModularMonolithApp/eshop.Orders/Modules/Orders/Endpoints.cs
@@ -1,6 +1,7 @@
 ﻿using eshop.Orders.Modules.Orders.Models;
 using eshop.Orders.Modules.Orders.PublicApi;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.OpenApi.Models;
 
 namespace eshop.Orders.Modules.Orders
 {
@@ -8,22 +9,49 @@ namespace eshop.Orders.Modules.Orders
     {
         public static void MapOrdersEndpoints(this IEndpointRouteBuilder route)
         {
-            var app = route.MapGroup("api/orders").WithOpenApi().WithTags("Orders");
+            var app = route.MapGroup("api/orders")
+                .WithOpenApi(operation=> new OpenApiOperation(operation)
+                    {
+                        Summary = "Order Management API", // Group-level summary
+                        Description = "APIs for creating and retrieving orders" // Group level description
+                    })
+                .WithTags("Orders");
+
             app.MapPost("", async (CreateOrderDto order, IOrderService orderService) =>
             {
                 var result = await orderService.CreateOrder(order);
 
                 return Results.Created($"orders/{result.Id}", result);
-            });
+            })
+            .WithName("createOrder") // Named endpoint for URL generation
+            .WithDescription("Create a new order in the system") // Endpoint description
+            .WithSummary("Create a new Order") // Brief Summary
+            .WithOpenApi(operation =>
+            {
+                operation.RequestBody = new OpenApiRequestBody
+                {
+                    Content = { ["application/json"]=new OpenApiMediaType
+                    {
+                        Schema = new OpenApiSchema
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.Schema,
+                                Id = "CreateOrderDto" // References to DTO schema
+                            }
+                        }
+                    }
+                }
+                };
+                return operation;
+            })
+            .Produces<Order>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest);
 
             app.MapGet("/{id:guid}", async (Guid id, IOrderService orderService) =>
             {
                 var order = await orderService.GetOrder(id);
-                if(order==null)
-                {
-                    return Results.NotFound();
-                }
-                return Results.Ok(order);
+                return order is null ? Results.NotFound() : Results.Ok(order);                
             });
         }
     }
