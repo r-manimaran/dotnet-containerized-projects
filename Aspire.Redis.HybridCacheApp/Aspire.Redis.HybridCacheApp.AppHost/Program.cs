ffed05ad12db283d60e108bd5c6e2456feda8f97
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-04-17 21:00:37 -0400

    Updated App.Host version

--- Aspire.Redis.HybridCacheApp/Aspire.Redis.HybridCacheApp.AppHost/Program.cs
+++ Aspire.Redis.HybridCacheApp/Aspire.Redis.HybridCacheApp.AppHost/Program.cs
@@ -4,6 +4,7 @@ var redis = builder.AddRedis("redis");
 
 builder.AddProject<Projects.Aspire_Redis_HybridCache>("aspire-redis-hybridcache")
     .WithReference(redis)
-    .WaitFor(redis);
+    .WaitFor(redis)
+    .WithReplicas(2);
 
 builder.Build().Run();
