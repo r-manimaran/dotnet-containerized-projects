b34d3efccc67a3205371ec8898912f7537dba76b
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-04-23 21:58:43 -0400

    updated Aspire version

---
+++ Aspire.eShop/Aspire.eshop.BasketService/Models/DecimalValue.cs
@@ -0,0 +1,24 @@
+﻿namespace Aspire.eshop.BasketService.Models;
+
+public partial class DecimalValue
+{
+    private const decimal NanoFactor = 1_000_000_000;
+    public DecimalValue(long units, int nanos)
+    {
+        //Units = units;
+        //Nanos = nanos;
+    }
+
+    public static implicit operator decimal(DecimalValue grpcDecimal)
+    {
+        return 0;
+        //return grpcDecimal.Units + grpcDecimal.Nanos / NanoFactor;
+    }
+
+    public static implicit operator DecimalValue(decimal value)
+    {
+        var units = decimal.ToInt64(value);
+        var nanos = decimal.ToInt32((value - units) * NanoFactor);
+        return new DecimalValue(units, nanos);
+    }
+}
