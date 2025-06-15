85c56da027fd637dfd00106e8e8b5159c90985c1
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-06-15 12:31:44 -0400

    Added Azure Storage Hosting service in Aspire Host and tested the Image upload to Azure container

--- AppreciationApp/AppreciationApp.AppHost/Program.cs
+++ AppreciationApp/AppreciationApp.AppHost/Program.cs
@@ -6,27 +6,34 @@ var postgres = builder.AddPostgres("postgres")
                        .AddDatabase("Appreciation");
 
 var username = builder.AddParameter("username", "admin");
+
 var password = builder.AddParameter("password","admin", secret: true);
 
-var keycloak = builder.AddKeycloak("keycloak")
+var blobs = builder.AddAzureStorage("storage")
+                   .RunAsEmulator()
+                   .AddBlobs("blobs");
+
+
+var keycloak = builder.AddKeycloak("keycloak", 8082)
     .WithDataVolume()
     .WithExternalHttpEndpoints()
     .WithRealmImport("./KeycloakConfiguration");
+
     
 var api = builder.AddProject<Projects.AppreciateAppApi>("appreciateappapi")
                 .WithExternalHttpEndpoints()
                  .WithReference(postgres)
                  .WaitFor(postgres)
                  .WithReference(keycloak)
-                  .WithEnvironment("Keycloak__ClientId", "confidential-client")
-            .WithEnvironment("keycloak__ClientSecret", "ze4SQDpbyBlB72kdTCTv8ecSWsJHf2Js");
+                 .WithEnvironment("Keycloak__ClientId", "confidential-client")
+                 .WithEnvironment("keycloak__ClientSecret", "ze4SQDpbyBlB72kdTCTv8ecSWsJHf2Js")
+                 .WithReference(blobs);
 
 builder
     .AddNpmApp("AngularFrontEnd", "../Appreciation.Web")
     .WithReference(api)
     .WithEndpoint(4200,scheme:"http",env:"PORT")
     .PublishAsDockerFile();
-
     
 
 builder.Build().Run();
