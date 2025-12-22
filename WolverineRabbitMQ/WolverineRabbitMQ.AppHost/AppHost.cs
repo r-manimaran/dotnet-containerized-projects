4273dcc7394079ad2d73129e203b9a2fce0d135b
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-12-21 19:28:07 -0500

    Added RabbitMQ

--- WolverineRabbitMQ/WolverineRabbitMQ.AppHost/AppHost.cs
+++ WolverineRabbitMQ/WolverineRabbitMQ.AppHost/AppHost.cs
@@ -1,11 +1,20 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres")
                       .WithDataVolume()
                       .AddDatabase("user-mgmt");
+                     
+
+var rmq = builder.AddRabbitMQ("rmq")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithManagementPlugin();
 
 builder.AddProject<Projects.UserManagementApi>("usermanagementapi")
     .WithReference(postgres)
-    .WaitFor(postgres);
+    .WaitFor(postgres)
+    .WithReference(rmq)
+    .WaitFor(rmq);
 
 builder.Build().Run();
